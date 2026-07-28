44dc49c80fe8ec4781ed05bb379f169a8cc850f4
BhojGC <gcbhoj@gmail.com>
2026-07-27 23:34:20 -0400

    - initialized header text part

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,25 +1,44 @@
-import ProfileImage from "../UI/profileImage";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchResume } from "../features/resumeSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const resume = useSelector((state) => state.resume);
+
+  useEffect(() => {
+    dispatch(fetchResume());
+  }, [dispatch]);
+
+  const splitName = (resume?.name || "").split("");
+
   return (
     <div
-      className="container-fluid border border-2 border-danger mt-5 d-flex align-items-center justify-content-between px-5 libertinus-math-regular"
+      className="container-fluid border border-2 border-danger mt-3 px-5 py-5 libertinus-math-regular"
       style={{
         height: "250px",
         borderRadius: "15px",
-        fontSize: "18px",
-        boxShadow: "5px -10px 5px gray"
+        boxShadow: "5px -10px 5px gray",
       }}
     >
-      {/* Name and Email Section */}
-      <div>
-        <h2 className="mb-2">Bhoj GC</h2>
+      <div className="row h-100">
+        {/* Name and Contact */}
+        <div className="col-md-8 d-flex flex-column justify-content-center">
+          <h2>
+            {splitName.map((letter, index) => (
+              <span key={index}>{letter}</span>
+            ))}
+          </h2>
 
-        <p className="mb-0">ghartich@gmail.com</p>
+          <h4>{resume?.alias}</h4>
+
+          <p>{resume?.primaryEmail}</p>
+          <p>{resume?.contact}</p>
+        </div>
+
+        {/* Globe */}
+        <div className="col-md-4 d-flex justify-content-center align-items-center"></div>
       </div>
-
-      {/* Profile Image Section */}
-      <ProfileImage />
     </div>
   );
 };
