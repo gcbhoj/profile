806f8de69246b297ef3ce0867bfd50ea0e35aa4d
BhojGC <gcbhoj@gmail.com>
2026-08-02 20:04:24 -0400

    - added experience to home page

---
+++ src/components/webExperience.jsx
@@ -0,0 +1,39 @@
+const WebExperience = () => {
+  const progWorkExp = [
+    {
+      id: 1,
+      type: "Co-Op Work Term",
+      term: "3 Terms",
+    },
+    {
+      id: 2,
+      type: "Production Coordinator",
+      term: "2 Years",
+    },
+    {
+      id: 3,
+      type: "Team Lead",
+      term: "2 Years",
+    },
+  ];
+
+  return (
+    <div className="container-fluid py-3">
+      <div className="row g-4">
+        {progWorkExp.map((experience) => (
+          <div key={experience.id} className="col-12 col-md-6 col-lg-4">
+            <div className="card shadow h-100">
+              <div className="card-body text-center">
+                <h5 className="card-title">{experience.type}</h5>
+
+                <p className="card-text">{experience.term}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WebExperience;
