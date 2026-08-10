adc01e954e8e880a8bf21729fb2b9e5bfd464660
BhojGC <gcbhoj@gmail.com>
2026-08-09 22:40:48 -0400

    - remoned

--- src/config/gameList.js
+++ src/config/gameList.js
@@ -1,9 +1,1 @@
-import TicTacToe from "../games/ticTacToe";
-export const GameList = [
-  {
-    id: "tic-tac-toe",
-    name: "Tic Tac Toe",
-    thumbnail: "/tic-tac-toe.png",
-    component: TicTacToe,
-  },
-];
+export const GameList = [];
